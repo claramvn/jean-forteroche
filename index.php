c4db6538fa3a6ebc2482df64973e22b357b650a2
Clara Morvan <claramvnbrg@gmail.com>
2020-07-19 23:23:14 +0200

    Add page Home to router

--- index.php
+++ index.php
@@ -5,13 +5,22 @@ ini_set('error_reporting', E_ALL);
 
 require_once('vendor/autoload.php');
 
+use \App\Controller\FrontController;
+
 try {
     if (isset($_GET['action'])) {
-        switch ($action) {
+        switch ($_GET['action']) {
+
+            /*********************************************************************************
+            ************************************ FrontController *****************************
+            *********************************************************************************/
             
-          
+            
+        }
     } else {
         //Accueil
+        $frontController = new FrontController();
+        $frontController->home();
     }
 } catch (Exception $e) {
     echo $e->getMessage();
