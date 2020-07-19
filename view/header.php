4d6d12223604c92cbe81b5060e2476b58dd22f1f
Clara Morvan <claramvnbrg@gmail.com>
2020-07-19 23:18:12 +0200

    Add header frontend

---
+++ view/header.php
@@ -0,0 +1,33 @@
+<header>
+    <div id="header">
+        <div id="div_pages">
+            <nav>
+                <ul>
+                    <li><a href="index.php"><img id="tag" src="public/img/tag.png" alt="Logo Jean Forteroche" /></a>
+                    </li>
+                    <li><a href="index.php"><i class="fas
+                                fa-grip-lines-vertical"></i> ACCUEIL</a></li>
+                    <li><a href="index.php?action=author"><i class="fas
+                                fa-grip-lines-vertical"></i> L' AUTEUR</a>
+                    </li>
+                    <li><a href="index.php?action=posts"><i class="fas
+                                fa-grip-lines-vertical"></i> LES
+                            CHAPITRES</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div id="div_user">
+            <nav>
+                <ul class="second_nav_hover">
+                    <li>
+                        <i class="fas fa-user"></i>
+                        <ul class="second_ul">
+                            <li class="first_li"><a href="index.php?action=connection">CONNEXION</a></li>
+                            <li><a href="index.php?action=register">INSCRIPTION</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
