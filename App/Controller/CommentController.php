dfd72043d083d9e9189e9cd0cf2b4ffde0bca281
Clara Morvan <claramvnbrg@gmail.com>
2020-07-20 17:46:03 +0200

    features for add comment / report comment / display reported comments

--- App/Controller/CommentController.php
+++ App/Controller/CommentController.php
@@ -26,12 +26,50 @@ class CommentController extends AncestorController
                     header('Location: index.php?action=post&id=' . $postId . '#block_comment');
                 } else {
                     $_SESSION['success_com'] = "Votre commentaire a bien été ajouté";
-                    header('Location: index.php?action=post&id=' . $postId . '#block_comment');
+                    header('Location: index.php?action=getPost&id=' . $postId . '#block_comment');
                 }
             } else {
                 $_SESSION['error_com'] = "Le champ est nécessaire";
-                header('Location: index.php?action=post&id=' . $postId . '#block_comment');
+                header('Location: index.php?action=getPost&id=' . $postId . '#block_comment');
             }
         }
+    }
+
+    // Signaler un commentaire
+    public function reportComment()
+    {
+        $commentManager = new CommentManager();
+
+        $commentId = $this->cleanParam($_GET['id']);
+        $episodeId = $this->cleanParam($_GET['id_chapter']);
+
+        $reportComment = $commentManager->reportComment($commentId);
+
+        if ($reportComment === false) {
+            $_SESSION['error_com'] = "Impossible de signaler le commentaire";
+            header('Location: index.php?action=getPost&id=' . $episodeId . '#block_comment');
+        } else {
+            $_SESSION['success_com'] = "Votre commentaire a bien été signalé, merci.";
+            header('Location: index.php?action=getPost&id=' . $episodeId . '#block_comment');
+        }
+    }
+
+    /*********************** BACKEND ************************/
+
+    // Affichage commentaire(s) signalé(s)
+    public function adminReportedComments()
+    {
+        $commentManager = new CommentManager();
+
+        $reportedComments = $commentManager->getReportedComments();
+
+        $countReportedComments = count($reportedComments);
+
+        if ($reportedComments === false) {
+            $_SESSION['error_comment'] = "Impossible d'afficher les commentaire(s) signalé(s)";
+            header('Location: index.php?action=adminReportedComments');
+        }
+
+        require('view/adminReportedComments.php');
     }
 }
