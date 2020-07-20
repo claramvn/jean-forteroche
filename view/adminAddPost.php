6c97d0af9e567a66fb44bfc6ff86527b8a1c3098
Clara Morvan <claramvnbrg@gmail.com>
2020-07-20 22:40:32 +0200

    bug input title and content

--- view/adminAddPost.php
+++ view/adminAddPost.php
@@ -9,31 +9,29 @@ $title = 'Jean Forteroche - Créer un chapitre';
 <div class="content">
 
     <!--Créer un  chapitre -->
-    <div class="block_dash">
-        <h1>Créer un chapitre</h1><br />
+    <h1>Créer un chapitre</h1><br />
 
-        <!-- Formulaire -->
-        <form id="form_create" action="index.php?action=adminAddPost" method="post" enctype="multipart/form-data">
+    <!-- Formulaire -->
+    <form id="form_create" action="index.php?action=adminAddPost" method="post" enctype="multipart/form-data">
 
-            <label for="file_chapter">IMAGE : <input type="file" name="file_chapter" required /></label><br />
-            <label for="title_chapter">CHAPITRE</label><br />
-            <input type="text" name="title_chapter" placeholder="Chapitre VI"
-                value="<?= $titlePost ?>" required><br /><br />
-            <label for=" content_chapter">CONTENU</label><br />
-            <textarea class="textarea_tiny"
-                name="content_chapter"><?= $text ?></textarea><br /><br />
-            <input id="button_form" type="submit" name="button_create_chapter" value="PUBLIER LE CHAPITRE" />
+        <label for="file_chapter">IMAGE : <input type="file" name="file_chapter" required /></label><br />
+        <label for="title_chapter">CHAPITRE</label><br />
+        <input type="text" name="title_chapter" placeholder="Chapitre VI"
+            value="<?= $titlePost ?>" required><br /><br />
+        <label for=" content_chapter">CONTENU</label><br />
+        <textarea class="textarea_tiny"
+            name="content_chapter"><?= $text ?></textarea><br /><br />
+        <input id="button_form" type="submit" name="button_create_chapter" value="PUBLIER LE CHAPITRE" />
 
-            <!-- Messages d'erreurs -->
-            <?php
+        <!-- Messages d'erreurs -->
+        <?php
             if (!empty($errors)) { ?>
-            <div class="error"><i class="fas fa-times"></i><br /><?= implode('<br/>', $errors) ?>
-            </div><br /><br />
-            <?php
+        <div class="error"><i class="fas fa-times"></i><br /><?= implode('<br/>', $errors) ?>
+        </div><br /><br />
+        <?php
             }
             ?>
-        </form>
-    </div>
+    </form>
 </div>
 
 <?php $content = ob_get_clean(); ?>
