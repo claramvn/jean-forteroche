1f9f367196138020fd8a865b3b3536087f4874a5
Clara Morvan <claramvnbrg@gmail.com>
2020-07-20 15:07:58 +0200

    header authentification

---
+++ view/auth_header.php
@@ -0,0 +1,21 @@
+<header>
+    <div id="header_admin">
+        <div>
+            <a href="index.php"><img src="public/img/loginLogo.png" alt="Logo Jean Forteroche" /></a>
+        </div>
+        <div>
+            <nav>
+                <ul>
+                    <li id="<?= $admin_li1 ?>"><a
+                            href="index.php?action=register">CRÉER
+                            UN COMPTE</a>
+                    </li>
+                    <li id="<?= $admin_li2 ?>"><a
+                            href="index.php?action=connection">SE
+                            CONNECTER</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
