edf3f9b6f3f7e148fa313e5126e6a88144622469
Clara Morvan <claramvnbrg@gmail.com>
2020-07-20 15:09:21 +0200

    page register

---
+++ view/register.php
@@ -0,0 +1,49 @@
+<?php
+
+// Titre site
+$title = 'INSCRIPTION';
+
+// Classe inscription
+$admin_li1 = 'inscription_li1';
+$admin_li2 = 'inscription_li2';
+?>
+
+<!-- Content -->
+<?php ob_start(); ?>
+
+<!-- header -->
+<?php
+require('auth_header.php');
+?>
+
+<!-- Block Formulaire -->
+<div id="form">
+
+    <!-- Messages d'erreurs -->
+    <?php
+        if (!empty($errors)) {
+            ?>
+    <div class="error"><i class="fas fa-times"></i><br /><?= implode('<br/>', $errors) ?>
+    </div><br /><br />
+    <?php
+        }
+    ?>
+
+    <!-- Formulaire -->
+    <form action="index.php?action=register" method="post">
+        <label for="pseudo_register">NOM D'UTILISATEUR</label><br />
+        <input type="text" name="pseudo_register" value="" required /><br /><br />
+        <label for="email_register">ADRESSE E-MAIL</label><br />
+        <input type="text" name="email_register" placeholder="contact@nomdedomaine.com" value="" required /><br /><br />
+        <label for="pass_register">MOT DE PASSE</label><br />
+        <input type="password" name="pass_register" placeholder="minimum 6 caractères" required /><br /><br />
+        <label for="pass2_register">CONFIRMATION DE MOT DE PASSE</label><br />
+        <input type="password" name="pass2_register" required /><br /><br />
+        <input id="button_form" type="submit" name="button_register" value="ENVOYER" />
+    </form>
+</div>
+
+<div class="auth_space"></div>
+
+<?php $content = ob_get_clean(); ?>
+<?php require('view/template.php');
