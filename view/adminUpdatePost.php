05e400410e1a2fe6f069f336d1ed69a36b50e05d
Clara Morvan <claramvnbrg@gmail.com>
2020-07-20 22:37:45 +0200

    feature update post - backend

---
+++ view/adminUpdatePost.php
@@ -0,0 +1,69 @@
+<?php
+// Titre site
+$title = 'Jean Forteroche - Modifier un chapitre';
+?>
+
+<!-- Content -->
+<?php ob_start(); ?>
+
+
+<div class="content">
+
+    <!--Créer un  chapitre -->
+    <h1>Modifier un chapitre</h1>
+
+
+    <form id="form_create"
+        action="index.php?action=adminUpdatePost&amp;id=<?= htmlspecialchars($post['id_chapter']) ?>"
+        method="post" enctype="multipart/form-data">
+        <label for="file_chapter">IMAGE :</label><br />
+        <img class="img_update"
+            src="public/img/<?= htmlspecialchars($image) ?>"
+            alt="Billet simple pour l'Alaska - Jean Forteroche" /><br /><br />
+        <input type="file" name="file_chapter" /><br /><br />
+        <label for="title_chapter">CHAPITRE</label><br />
+        <input type="text" name="title_chapter"
+            value="<?= htmlspecialchars($titlePost) ?>"
+            required><br /><br />
+        <label for=" content_chapter">CONTENU</label><br />
+        <textarea class="textarea_tiny"
+            name="content_chapter"><?= $text ?></textarea><br /><br />
+        <label>DATE DE PUBLICATION :
+            <?php
+                    $dateFr = $this->dateTimeUsToDateTimeFr(htmlspecialchars($date));
+                    echo $dateFr; ?>
+            <a class="btn_input" href="#form_create">MODIFIER <span class="red">
+                    <i class=" fas fa-chevron-right"></i><i class="fas fa-chevron-right"></i>
+                </span></a></label>
+        <div id="publication">
+            <input class="date_input" type="date" name="date_chapter"
+                max="<?php echo date("Y-m-d");?>"
+                value="<?php
+                    $dateUs = $this->getdateOfDateTimeUs($date);
+                    echo $dateUs;
+                    ?>" disabled />
+            <input class="time_input" type="time" name="time_chapter" step="2" value="<?php
+                    $timeUs = $this->getTimeOfDateTimeUs($date);
+                    echo $timeUs;
+                    ?>" disabled />
+        </div><br /><br />
+        <input id="button_form" type="submit" name="button_update_chapter" value="PUBLIER LE CHAPITRE" />
+
+        <!-- Messages d'erreurs & succès -->
+        <?php
+            if (!empty($errors)) { ?>
+        <div class="error"><i class="fas fa-times"></i><br /><?= implode('<br/>', $errors) ?>
+        </div><br /><br />
+        <?php
+            }
+            if (!empty($success)) { ?>
+        <div class="success"><i class="fas fa-check"></i><br /><?= implode('<br/>', $success) ?>
+        </div><br /><br />
+        <?php
+            }
+            ?>
+    </form>
+</div>
+
+<?php $content = ob_get_clean(); ?>
+<?php require('view/adminTemplate.php');
