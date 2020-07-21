bef70c82c3f970923a46be7aa07133bfde448078
Clara Morvan <claramvnbrg@gmail.com>
2020-07-21 12:37:54 +0200

    correction condition on update post

--- App/Controller/PostController.php
+++ App/Controller/PostController.php
@@ -143,81 +143,81 @@ class PostController extends AncestorController
     
         if ($post === false) {
             header('Location: index.php?action=adminListPosts');
-        }
+        } else {
+            $titlePost = $post['title_chapter'];
+            $text = $post['text_chapter'];
+            $image = $post['image_chapter'];
+            $date = $post['date_chapter'];
     
-        $titlePost = $post['title_chapter'];
-        $text = $post['text_chapter'];
-        $image = $post['image_chapter'];
-        $date = $post['date_chapter'];
+            $errors = [];
+            $success = [];
     
-        $errors = [];
-        $success = [];
-    
-        if (isset($_POST['button_update_chapter'])) {
-            $titlePost = $this->cleanParam($_POST['title_chapter']);
-            $text = $_POST['content_chapter'];
-
-            // Vérif champs vides
-            if (empty($titlePost) && empty($text)) {
-                $errors['empty_post'] = "- Tous les champs sont nécessaires";
-                $titlePost = $this->cleanParam($post['title_chapter']);
-                $text = $post['text_chapter'];
-            } else {
+            if (isset($_POST['button_update_chapter'])) {
                 $titlePost = $this->cleanParam($_POST['title_chapter']);
                 $text = $_POST['content_chapter'];
-            }
- 
-            // Traitement titre
-            if ($post['title_chapter'] !== $titlePost) {
-                $success['title_post'] = "- Le titre a bien été modifié";
-            } else {
-                $titlePost = $post['title_chapter'];
-            }
-        
-            // Traitement contenu : textarea
-            if ($post['text_chapter'] !== $text) {
-                $success['content_post'] = "- Le contenu a bien été modifié";
-            } else {
-                $text = $post['text_chapter'];
-            }
-    
-            // Traitement image
-            if (isset($_FILES["file_chapter"]) && $_FILES["file_chapter"]["error"] == 0) {
-                $file = $_FILES['file_chapter'];
-                $fileExtensionUpload = $this->fileExtensionUpload($file);
-                $fileExtensionAllowed = $this->fileExtensionAllowed();
-                $newName = $this->newName($file, $fileExtensionUpload);
-    
-                if (!$this->checkMaxSize($file)) {
-                    $errors['size_img_post'] = "- Le fichier est trop volumineux";
-                }
-    
-                if (!in_array($fileExtensionUpload, $fileExtensionAllowed)) {
-                    $errors['extension_img_post'] = "- Le fichier n'est pas au format jpg/jpeg/png/gif";
-                }
-    
-                if (!$errors) {
-                    $fileUpload = $this->uploadFile($file, $newName);
-                    $image = $newName;
-                    $success['img_post'] = "- L'image a bien été modifiée";
+
+                // Vérif champs vides
+                if (empty($titlePost) && empty($text)) {
+                    $errors['empty_post'] = "- Tous les champs sont nécessaires";
+                    $titlePost = $this->cleanParam($post['title_chapter']);
+                    $text = $post['text_chapter'];
                 } else {
-                    $image = $post['image_chapter'];
+                    $titlePost = $this->cleanParam($_POST['title_chapter']);
+                    $text = $_POST['content_chapter'];
                 }
-            }
+ 
+                // Traitement titre
+                if ($post['title_chapter'] !== $titlePost) {
+                    $success['title_post'] = "- Le titre a bien été modifié";
+                } else {
+                    $titlePost = $post['title_chapter'];
+                }
+        
+                // Traitement contenu : textarea
+                if ($post['text_chapter'] !== $text) {
+                    $success['content_post'] = "- Le contenu a bien été modifié";
+                } else {
+                    $text = $post['text_chapter'];
+                }
     
-            // Si modification date
-            if (isset($_POST['date_chapter']) && !empty($_POST['date_chapter']) && isset($_POST['time_chapter']) && !empty($_POST['time_chapter'])) {
-                $dateUs = $this->cleanParam($_POST['date_chapter']);
-                $time = $this->cleanParam($_POST['time_chapter']);
-                $date = $dateUs . " " . $time;
-                $success['date_post'] = "- La date a bien été modifiée";
-            }
+                // Traitement image
+                if (isset($_FILES["file_chapter"]) && $_FILES["file_chapter"]["error"] == 0) {
+                    $file = $_FILES['file_chapter'];
+                    $fileExtensionUpload = $this->fileExtensionUpload($file);
+                    $fileExtensionAllowed = $this->fileExtensionAllowed();
+                    $newName = $this->newName($file, $fileExtensionUpload);
     
-            $updatePost = $postManager->updatePost($titlePost, $text, $image, $date, $postId);
+                    if (!$this->checkMaxSize($file)) {
+                        $errors['size_img_post'] = "- Le fichier est trop volumineux";
+                    }
     
-            if ($updatePost === false) {
-                $errors['update_post'] = "Impossible de modifier le chapitre";
-                header('Location: index.php?action=adminListPosts');
+                    if (!in_array($fileExtensionUpload, $fileExtensionAllowed)) {
+                        $errors['extension_img_post'] = "- Le fichier n'est pas au format jpg/jpeg/png/gif";
+                    }
+    
+                    if (!$errors) {
+                        $fileUpload = $this->uploadFile($file, $newName);
+                        $image = $newName;
+                        $success['img_post'] = "- L'image a bien été modifiée";
+                    } else {
+                        $image = $post['image_chapter'];
+                    }
+                }
+    
+                // Si modification date
+                if (isset($_POST['date_chapter']) && !empty($_POST['date_chapter']) && isset($_POST['time_chapter']) && !empty($_POST['time_chapter'])) {
+                    $dateUs = $this->cleanParam($_POST['date_chapter']);
+                    $time = $this->cleanParam($_POST['time_chapter']);
+                    $date = $dateUs . " " . $time;
+                    $success['date_post'] = "- La date a bien été modifiée";
+                }
+    
+                $updatePost = $postManager->updatePost($titlePost, $text, $image, $date, $postId);
+    
+                if ($updatePost === false) {
+                    $errors['update_post'] = "Impossible de modifier le chapitre";
+                    header('Location: index.php?action=adminListPosts');
+                }
             }
         }
     
